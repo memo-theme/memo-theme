ebeca1f87b83f9f103934a3b402192b7247d6ae7
theme-memo <245191659+theme-memo@users.noreply.github.com>
2025-11-27 10:43:41 +0000

    feat: add cloudflare integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,14 +5,13 @@ import partytown from "@astrojs/partytown";
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  site: "https://332712.xyz",
+  site: process.env.SITE_HOST,
   base: "/",
   trailingSlash: "always",
   output: "server",
   prefetch: true,
   adapter: cloudflare({
     imageService: "cloudflare",
-    sessionKVBindingName: "THEME_MEMORIES_SESSION",
   }),
   vite: {
     plugins: [tailwindcss()],
